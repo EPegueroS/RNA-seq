288cfcb5daf27bc1f014f2cfd9c07dde5f83d923
Esteban Peguero-Sanchez <esteban.peguero@gmail.com>
2017-06-11 14:59:59 +0200

    Create google_sheet_functions.R

---
+++ google/google_sheet_functions.R
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------------------- #
+fetchGoogleSheet = function(key, prefix=NULL){
+
+    library(googlesheets)
+
+
+    if(is.null(prefix))
+        prefix='~/'
+
+    token_path = file.path('/local/vedran/Scripts/SeqeunceData/Counting/googlesheets_token_EPS.rds')
+    if(!file.exists(token_path)){
+        token <- gs_auth(cache = FALSE, key=key)
+        saveRDS(token, file = token_path)
+    }
+    suppressMessages(gs_auth(token = token_path, verbose = FALSE))
+    gap = gs_key(key)
+    return(gap)
+}
