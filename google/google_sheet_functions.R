1328acace60a7ee903cfe623310f99fbeac60898
Esteban Peguero-Sanchez <esteban.peguero@gmail.com>
2017-06-11 16:13:41 +0200

    Create google_sheet_functions.R

--- google/google_sheet_functions.R
+++ google/google_sheet_functions.R
@@ -1,21 +1,4 @@
 # ---------------------------------------------------------------------------- #
 
 ## this function fetches the google sheet containing the description of the RNA samples
-retrieve_sample_sheet = function(google_key, path=NULL){
-
-    library(googlesheets)
-
-    
-    if(is.null(path))
-        path='~/'
-
-    token_path = file.path(path, '/googlesheets_token_EPS.rds')
-    if(!file.exists(token_path)){
-        token <- gs_auth(cache = FALSE, key=google_key)
-        saveRDS(token, file = token_path)
-    }
-    suppressMessages(gs_auth(token = token_path, verbose = FALSE))
-    gap = gs_key(google_key)
-    return(gap)
-}
-
+# ---------------------------------------------------------------------------- #
